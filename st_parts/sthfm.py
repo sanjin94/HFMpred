ea50bee5c501c8c66104ee04a245f183e490b659
sanjin94 <sanjin.gumbarevic@gmail.com>
2022-03-11 15:54:44 +0100

    plot update

--- st_parts/sthfm.py
+++ st_parts/sthfm.py
@@ -16,23 +16,6 @@ import time
 import regression as worker
 
 
-#EMOJI_ICON = "icons/HFMpred.ico"
-#EMOJI_PNG = "icons/HFMpred.png"
-
-#st.set_page_config(page_title="HFMpred", page_icon=EMOJI_ICON,
-#                   layout='wide')
-
-#col1, col2 = st.columns(2)
-#with col2:
-#    st.image(EMOJI_PNG, width=80)
-#col1, col2 = st.columns([1.5,6])
-#with col2:
-#    st.title('HFMpred - a tool for HFM results analysis and prediction')
-#col1, col2 = st.columns([5, 6])
-#with col2:
-#    st.markdown('## U-value analysis')
-
-
 def app():
     torch.manual_seed(1618)
     uploaded_file = st.sidebar.file_uploader('Upload a file to input data', type='csv')
@@ -50,8 +33,7 @@ def app():
     select_optimizer = st.sidebar.selectbox('Select optimizer', ['Adam', 'SGD'])
     nepochs = st.sidebar.number_input('Specify number of epochs', min_value=1, value=50)
     model_name = st.sidebar.text_input('Specify model name', value=file_name[0:-4]+'_LSTM_b'+str(bsize)+'_nl'+str(nlayers)+'_pd'+str(dropout)[2:]+'_op'+select_optimizer)
-    avg_const = st.sidebar.number_input('Specify average constant:', min_value=1, max_value=60, value=1)
-    #regularization = st.sidebar.checkbox('Regularization', value=True)
+    avg_const = 1 #st.sidebar.number_input('Specify average constant:', min_value=1, max_value=60, value=1)
     model_save = st.sidebar.checkbox('Model save', value=False)
     run_button = st.sidebar.button('Run')
 
@@ -62,7 +44,6 @@ def app():
         split_list = data_worker.tv_split(v1_avg, v2_avg, bsize)
         v1_train = torch.tensor(split_list[0])
         v2_train = torch.tensor(split_list[1])
-
 
         if bsize > len(v1_train):
             st.warning('Batch size is greater than the vector length! bsize = len(vector)')
@@ -101,10 +82,6 @@ def app():
                     regulizer = regulizer.flatten()
                     st.warning('WARRNING! Device = "cpu"!')
                 loss = loss_fn(pred, regulizer[batch][0])
-                #if regularization == True:
-                #    l2_lambda = 0.001
-                #    l2_norm = torch.tensor(sum(p.pow(2.0).sum() for p in model.parameters())).to(device).float()
-                #    loss += l2_lambda * l2_norm
                 loss.backward()
                 optimizer.step()
                 loss_epoch.append(loss.item())
@@ -141,21 +118,47 @@ def app():
                 q_prediction = torch.flatten(q_prediction).cpu().detach().numpy()
                 q_pred = np.append(q_pred, q_prediction)
 
+        input_plt_name = model_name + '_input'
+        loss_plt_name = model_name + '_loss'
+        results_plt_name = model_name + '_results'
         fig, ax = plt.subplots()
-        fig.set_size_inches(8, 5)
+        fig.set_size_inches(13, 8)
+        ax.set_title('Ulazni podaci')
+        ax.set_xlabel('Vrijeme [minute]')
+        ax.set_ylabel('Razlika temperature [°C]')
+        ax2 = ax.twinx()
+        ax2.set_ylabel('Toplinski tok [$W / m^2$]')
         ax.scatter(split_list[2], split_list[0], s=0.2, color=(0.5, 0.2, 0.5))
-        ax.scatter(split_list[2], split_list[1], s=0.2, color=(0.1, 0.2, 0.5))
+        ax2.scatter(split_list[2], split_list[1], s=0.2, color=(0.1, 0.2, 0.5))
         ax.scatter(split_list[5], split_list[3], s=0.2, color=(0.5, 0.4, 0.5, 0.4))
-        ax.scatter(split_list[5], split_list[4], s=0.2, color=(0.1, 0.4, 0.5, 0.4))
+        ax2.scatter(split_list[5], split_list[4], s=0.2, color=(0.1, 0.4, 0.5, 0.4))
+        box = ax.get_position()
+        ax.set_position([box.x0, box.y0 + box.height * 0.2,
+                          box.width, box.height * 0.8])
+        ax.legend(['$\Delta T$ trening', '$\Delta T$ validacija'], loc='lower center', markerscale=5,
+                  bbox_to_anchor=(0.35, -0.35), fancybox=True)
+        ax2.legend(['q trening', 'q validacija'], loc='lower center', markerscale=5,
+                   bbox_to_anchor=(0.65, -0.35), fancybox=True)
+        plt.savefig('results/input_train_validation/' + input_plt_name)
 
-        col1, col2 = st.columns([3, 3])
-        with col1:
-            st.pyplot(fig)
+        col1, col2, col3 = st.columns([0.5, 3, 0.5])
         with col2:
-            fig, ax = plt.subplots()
-            fig.set_size_inches(8, 5)
-            ax.plot(loss_list, color=(0.5, 0.2, 0.5))
-            ax.plot(loss_val_list, color=(0.1, 0.2, 0.5, 0.6))
+            st.pyplot(fig)
+        fig, ax = plt.subplots()
+        fig.set_size_inches(13, 8)
+        ax.set_title('Gubitak treninga i validacije')
+        ax.set_xlabel('Epoha')
+        ax.set_ylabel('MSE gubitak')
+        ax.plot(loss_list, color=(0.5, 0.4, 0.5, 0.6))
+        ax.plot(loss_val_list, color=(0.1, 0.4, 0.5, 0.6))
+        box = ax.get_position()
+        ax.set_position([box.x0, box.y0 + box.height * 0.2,
+                         box.width, box.height * 0.8])
+        ax.legend(['Gubitak treninga', 'Gubitak validacije'], loc='lower center',
+                    bbox_to_anchor=(0.5, -0.35), fancybox=True)
+        plt.savefig('results/losses/' + loss_plt_name)
+        col1, col2, col3 = st.columns([0.5, 3, 0.5])
+        with col2:
             st.pyplot(fig)
 
         fig, ax = plt.subplots()
@@ -164,11 +167,24 @@ def app():
         ax.plot(q_pred, color=(0.5, 0.2, 0.5))
         u_m, U_m = worker.u_val(v1_comb.cpu().flatten().numpy(), v2_comb.cpu().flatten().numpy())
         u_p, U_p = worker.u_val(v1_comb.cpu().flatten().numpy(), q_pred)
+        ax.set_xlabel('Vrijeme [minute]')
+        ax.set_ylabel('Toplinski tok [$W / m^2$]')
         ax2 = ax.twinx()
-        ax2.set_ylim([-5, 5])
-        ax2.plot(u_m)
-        ax2.plot(u_p)
-        st.pyplot(fig)
+        ax2.set_ylim([-7, 7])
+        ax2.plot(u_m, color=(0.5, 0.4, 0.5, 0.4))
+        ax2.plot(u_p, color=(0.1, 0.4, 0.5, 0.4))
+        ax2.set_ylabel('U vrijednost [W/($m^2K$)]')
+        box = ax.get_position()
+        ax.set_position([box.x0, box.y0 + box.height * 0.2,
+                         box.width, box.height * 0.8])
+        ax.legend(['q predikcija', 'q mjereno'], loc='lower center', markerscale=5,
+                  bbox_to_anchor=(0.35, -0.35), fancybox=True)
+        ax2.legend(['U mjereno', 'U predikcija'], loc='lower center', markerscale=5,
+                   bbox_to_anchor=(0.65, -0.35), fancybox=True)
+        plt.savefig('results/predictions/' + results_plt_name)
+        col1, col2, col3 = st.columns([0.5, 3, 0.5])
+        with col2:
+            st.pyplot(fig)
         print(abs((U_m - U_p) / U_p) * 100)
 
         # Saving the model
